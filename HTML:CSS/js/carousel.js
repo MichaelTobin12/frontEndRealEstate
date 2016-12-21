8dcbe80ca0c36681d16a87161cdf5fc25522117f
Michael Tobin <mikel.tobin@gmail.com>
2016-12-21 00:10:37 -0700

    Fully responsive. Can't believe I haven't commited yet.

--- HTML:CSS/js/carousel.js
+++ HTML:CSS/js/carousel.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+
+
+	$(window).scroll(function() {
+		let scroll = $(window).scrollTop();
+		let navBarMarginFix = document.getElementsByClassName("marginBottomScrollChange")[0];
+		scroll > 9 ? $(navBarMarginFix).animate({marginBottom: "0px"}, 100) : $(navBarMarginFix).animate({marginBottom: "20px"}, 100);
+	});
+
 	$(function() {
 		$('#carousel-example-generic').carousel();
 	});
+
 });
