2639f5374d21cdfdd7ab8cbda326fcb337b62bf2
Michael Tobin <mikel.tobin@gmail.com>
2016-12-21 01:14:23 -0700

    Last commit before turning over

--- HTML:CSS/js/carousel.js
+++ HTML:CSS/js/carousel.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-
+	let contactForm = document.getElementById("contactFormId");
+	let firstName = document.getElementsByClassName("inputSize")[0];
+	let lastName = document.getElementsByClassName("inputSize")[1];
+	let email = document.getElementsByClassName("inputSize")[2];
+	let phone = document.getElementsByClassName("inputSize")[3];
+	let zip = document.getElementsByClassName("inputSize")[4];
+	let reason = document.getElementsByClassName("inputSize")[5];
+	let message = document.getElementById("userMessageId");
 
 	$(window).scroll(function() {
 		let scroll = $(window).scrollTop();
@@ -11,4 +18,27 @@ $(document).ready(function() {
 		$('#carousel-example-generic').carousel();
 	});
 
+	$(contactForm).submit(function(event) {
+		let contactObj = {};
+
+		contactObj.firstName = $(firstName).val();
+		contactObj.lastName = $(lastName).val();
+		contactObj.email = $(email).val();
+		contactObj.phone = $(phone).val();
+		contactObj.zip = $(zip).val();
+		contactObj.reason = $(reason).val();
+		contactObj.message = $(message).val();
+
+		$.ajax({
+			url: "michaeltobin.us/#/contact/incoming",
+			type: 'post',
+			data: contactObj,
+			success: function(data) {
+				console.log(data);
+			}
+		}).done(function() {
+			alert('Finished')
+		})
+		event.preventDefault();
+	})
 });
